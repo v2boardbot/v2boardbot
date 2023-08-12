2c10d34d17d3fe793f104c4aafd1b6a9dc4369ad
v2boardbot <zhuli8@protonmail.com>
2023-08-12 15:11:31 +0800

    20230812.3 dev

--- admin/utils.py
+++ admin/utils.py
@@ -2,6 +2,10 @@ from Config import config
 game_dict = {
     '🎰老虎机': config.TIGER,
     '🎲骰子': config.DICE,
+    '🏀篮球': config.BASKETBALL,
+    '⚽足球': config.FOOTBALL,
+    '🎯飞镖': config.BULLSEYE,
+    '🎳保龄球': config.BOWLING,
 }
 settings_dict = {
     '🏷️标题设置': 'title',
